cf0dda50f9e33dbccfc1722049fa1852c4cd7010
ryan191188 <ryan_zhong@mymail.sutd.edu.sg>
2019-05-21 12:09:16 +0800

    initial solos app, route to index view

---
+++ jmad/solos/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import resolve
+
+from solos.views import index
+
+class SolosUrlsTestCase(TestCase):
+
+    def test_root_url_uses_index_view(self):
+        """
+        Test that the root of the sites resolves to the
+        correct view function
+        """
+        root = resolve('/')
+        self.assertEqual(root.func, index)
